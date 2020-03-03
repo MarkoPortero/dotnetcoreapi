7502ee72d6e9cde44656f5d8993513ebcf63bb38
Mark Porter <mark@porter.rocks>
2020-03-03 23:11:59 +0000

    Marks first commit

---
+++ MarksWebApp/Models/ToodoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarksWebApp.Models
+{
+    public class ToodoContext : DbContext
+    {
+        public ToodoContext(DbContextOptions<ToodoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
